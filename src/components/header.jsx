88c191327618b8d4637d93a5938d44988fc27403
Jacob Chase <jacobchase7@outlook.com>
2025-04-16 14:11:14 -0500

    initial commit

---
+++ src/components/header.jsx
@@ -0,0 +1,8 @@
+
+export default function header(){
+    return(
+        <>
+        <header><img src = "/src/assets/globe.png" className="globe" /><span>my travel journal.</span></header>
+        </>
+    )
+}
