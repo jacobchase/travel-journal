8853e7484880518afd624dfa2699fe460b8faaaa
Jacob Chase <jacobchase7@outlook.com>
2025-04-16 16:00:35 -0500

    created an entry component and added an example

---
+++ src/components/entry.jsx
@@ -0,0 +1,18 @@
+export default function entry(){
+    return(
+        <article>
+    <div>
+    <img src ="https://scrimba.com/links/travel-journal-japan-image-url" className="picture"/>
+    </div>
+    <div className = "text">
+    <span classname = "Location"><img src = "/src/assets/marker.png" className="marker"/>JAPAN <a href ="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">View on Google Maps</a></span>
+    <br></br>
+    <span className="Title">Mount Fuji</span>
+    <br></br>
+    <h4 className = "dates">12 Jan, 2021 - 24 Jan, 2021</h4>
+    <br></br>
+    <h5 className = "description">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</h5>
+    </div>
+    </article>
+    )
+}
